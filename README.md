5130ea93b5744d443100ac1f98e361c0d5e9b89a
thisisaaronland <aaron@mapzen.com>
2015-10-21 17:42:02 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# py-mapzen-whosonfirst-pip
+
+Simple Python wrapper for talking to the go-whosonfirst-pip server
+
+## IMPORTANT
+
+This library is provided as-is, right now. It lacks proper
+documentation which will probably make it hard for you to use unless
+you are willing to poke and around and investigate things on your
+own.
+
+## See also
+
+* https://github.com/whosonfirst/go-whosonfirst-pip
+
+
---
+++ mapzen/whosonfirst/pip/__init__.py
@@ -0,0 +1,38 @@
+# https://pythonhosted.org/setuptools/setuptools.html#namespace-packages
+__import__('pkg_resources').declare_namespace(__name__)
+
+import requests
+import json
+
+class server:
+
+    def __init__(self, **kwargs):
+
+        self.scheme = kwargs.get('scheme', 'http')
+        self.hostname = kwargs.get('hostname', 'localhost')
+        self.port = kwargs.get('port', 8080)
+
+    def reverse_geocode(self, lat, lon, placetype=None):
+
+        url = self.scheme + "://" + self.hostname
+
+        if self.port:
+            url = url + ":%s" % self.port
+
+        params = { "latitude": lat, "longitude": lon }
+        
+        if placetype:
+            params[ "placetype" ] = placetype
+
+        rsp = requests.get(url, params=params)
+        data = json.loads(rsp.content)
+
+        return data
+
+if __name__ == '__main__':
+
+    s = server()
+    r = s.reverse_geocode(40.677524,-73.987343)
+    
+    print r
+    
