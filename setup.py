5edd5164c63549bd43d97f9e576915cfa20a7c3b
Dan Phiffer <dan@phiffer.org>
2016-01-19 16:08:40 -0800

    🔧 Added VERSION to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
+
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.pip.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.pip',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.pip'],
-    version='0.3',
+    version=version,
     description='Simple Python wrapper for talking to the go-whosonfirst-pip server',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-pip',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-pip',
     install_requires=[
         'requests',
         ],
@@ -19,5 +32,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-pip/releases/tag/v0.3',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-pip/releases/tag/' + version,
     license='BSD')
