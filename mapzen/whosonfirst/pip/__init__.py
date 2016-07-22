e5c7940c8ad34bbbd96f4b72268510b0896517ff
thisisaaronland <whosonfirst@mapzen.com>
2016-07-22 23:37:15 +0000

    add kwargs to account for moar filtering in wof-pip-server

--- mapzen/whosonfirst/pip/__init__.py
+++ mapzen/whosonfirst/pip/__init__.py
@@ -6,12 +6,12 @@ import json
 
 class base:
 
-    def point_in_poly(self, endpoint, lat, lon, placetype=None):
+    def point_in_poly(self, endpoint, lat, lon, **kwargs):
 
         params = { "latitude": lat, "longitude": lon }
         
-        if placetype:
-            params[ "placetype" ] = placetype
+        for k, v in kwargs.items():
+            params[k] = v
 
         rsp = requests.get(endpoint, params=params)
         data = json.loads(rsp.content)
@@ -28,14 +28,14 @@ class server(base):
         self.hostname = kwargs.get('hostname', 'localhost')
         self.port = kwargs.get('port', 8080)
 
-    def reverse_geocode(self, lat, lon, placetype=None):
+    def reverse_geocode(self, lat, lon, **kwargs):
 
         url = self.scheme + "://" + self.hostname
 
         if self.port:
             url = url + ":%s/" % self.port
 
-        return self.point_in_poly(url, lat, lon,placetype)
+        return self.point_in_poly(url, lat, lon, **kwargs)
 
 # https://github.com/whosonfirst/go-whosonfirst-pip#wof-pip-proxy
 
@@ -47,7 +47,7 @@ class proxy(base):
         self.hostname = kwargs.get('hostname', 'localhost')
         self.port = kwargs.get('port', 1111)
 
-    def reverse_geocode(self, target, lat, lon, placetype=None):
+    def reverse_geocode(self, target, lat, lon, **kwargs):
 
         url = self.scheme + "://" + self.hostname
 
@@ -56,7 +56,7 @@ class proxy(base):
 
         url = url + target
 
-        return self.point_in_poly(url, lat, lon,placetype)
+        return self.point_in_poly(url, lat, lon, **kwargs)
 
 if __name__ == '__main__':
 
